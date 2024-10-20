3e51de85a4508b4ab9f61d512a12217d91adc3c3
hungtd412 <tdhung41204@gmail.com>
2024-10-20 15:43:49 +0700

    add email to users table

--- database/script.sql
+++ database/script.sql
@@ -1,15 +1,20 @@
 use lensart_eyewear
 
-insert into role(name) values
-('admin'),
-('manager'),
-('user')
+-- insert into role(name) values
+-- ('admin'),
+-- ('manager'),
+-- ('user')
 
 --insert into users(username, password, role_id, phone, address) values ('hungadmin', '123456', 1, '0987654321', '246 Trần Hưng Đạo, thị xã Quảng Trị, tỉnh Quảng Trị')
 
 select * from users
+
 desc users
 
-update users
-set role_id = 3
-where id = 2
+UPDATE users
+SET email = 'tdhung41204@gmail.com'
+WHERE id = 3;
+
+
+
+
