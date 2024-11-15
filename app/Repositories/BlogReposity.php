cc157594caceab38a760df3588c796b6bd1ab8d4
trinhtrinh342004 <121794416+trinhtrinh342004@users.noreply.github.com>
2024-11-15 16:20:15 +0700

    insert BlogController - Service - Reposity

---
+++ app/Repositories/BlogReposity.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Blog;
+
+class BlogReposity
+{
+    public function getActiveBlogs($limit = 10)
+    {
+        return Blog::where('status', 'active')
+            ->orderBy('id', 'desc')
+            ->take($limit)
+            ->get();
+    }
+}
