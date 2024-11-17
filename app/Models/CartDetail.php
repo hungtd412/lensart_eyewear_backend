a7ebcfc7cad5b82c954b02798b5c1339d83c062f
trinhtrinh342004 <121794416+trinhtrinh342004@users.noreply.github.com>
2024-11-17 17:34:33 +0700

    Update CartDetail.php

--- app/Models/CartDetail.php
+++ app/Models/CartDetail.php
@@ -9,18 +9,8 @@ class CartDetail extends Model
 {
     use HasFactory;
 
-    // Khai báo tên bảng (tùy chọn nếu tên bảng khác với tên model)
     protected $table = 'cart_details';
-
-    // Tắt timestamps nếu không sử dụng
     public $timestamps = false;
-
-    // Đặt khóa chính phức hợp
-    protected $primaryKey = ['cart_id', 'product_id', 'branch_id', 'color'];
-    public $incrementing = false;
-    protected $keyType = 'string';
-
-    // Các cột có thể điền vào (fillable)
     protected $fillable = [
         'cart_id',
         'product_id',
@@ -33,15 +23,5 @@ class CartDetail extends Model
     public function getTotalPriceAttribute()
     {
         return $this->quantity * ($this->product->offer_price ?? $this->product->price);
-    }
-
-    public function getIncrementing()
-    {
-        return false;
-    }
-
-    public function getKeyType()
-    {
-        return 'string';
     }
 }
