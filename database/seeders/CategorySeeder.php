831c17a639a06acb82e2a13d43aba3bb6ebb0fe5
hungtd412 <tdhung41204@gmail.com>
2024-12-19 13:42:54 +0700

    remove des in cate seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -13,15 +13,12 @@ class CategorySeeder extends Seeder {
         DB::table('category')->insert([
             [
                 'name' => 'Tròng kính',
-                'description' => 'Các loại tròng kính cho các nhu cầu khác nhau như cận thị, viễn thị, chống ánh sáng xanh và bảo vệ mắt.',
             ],
             [
                 'name' => 'Gọng kính',
-                'description' => 'Gọng kính đa dạng kiểu dáng, chất liệu và màu sắc, giúp định hình và giữ tròng kính chắc chắn.',
             ],
             [
                 'name' => 'Kính râm',
-                'description' => 'Kính râm bảo vệ mắt khỏi ánh sáng mặt trời và tia UV, thiết kế thời trang và tiện dụng.',
             ],
         ]);
     }
