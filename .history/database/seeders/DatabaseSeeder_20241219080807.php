67a7481844839be13ae77944c6f2841d6c9586fb
Nguyen Ba Hung <22520512@gm.uit.edu.vn>
2024-12-19 08:16:55 +0700

    add database seeder for initial data population

---
+++ .history/database/seeders/DatabaseSeeder_20241219080807.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder {
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void {
+        $this->call([
+            RoleSeeder::class,
+            UserSeeder::class,
+            // CategorySeeder::class,
+            // AttributesProductSeeder::class,
+            // BranchSeeder::class,
+            // ProductAndDetailSeeder::class,
+            // ProductFeatureSeeder::class,
+            // CouponSeeder::class,
+            // CartAndDetailSeeder::class,
+            // OrderAndDetailSeeder::class,
+            // WishlistAndDetailSeeder::class,
+            // BlogSeeder::class,
+            // ProductReviewSeeder::class,
+            // ProductImageSeeder::class, tam thoi chua seed product images
+        ]);
+    }
+}
