590cc0726f6d98257aa7f1e88e8939130a020762
Nguyen Ba Hung <22520512@gm.uit.edu.vn>
2024-12-21 09:17:23 +0700

    add StoreCartDetailRequest for validating cart detail submissions

---
+++ .history/app/Http/Requests/Cart/StoreCartDetailRequest_20241221091014.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Cart;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Traits\FailedValidationTrait;
+
+class StoreCartDetailRequest extends FormRequest {
+    use FailedValidationTrait;
+
+    /**
+     * Xác định xem người dùng có được ủy quyền để thực hiện yêu cầu này hay không.
+     */
+    public function authorize(): bool {
+        return true;
+    }
+
+    /**
+     * Lấy các quy tắc xác thực áp dụng cho yêu cầu.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array {
+        return [
+            // 'cart_id' => 'required|integer|exists:carts,id',
+            'product_id' => 'required|integer|exists:products,id',
+            'branch_id' => 'required|integer|exists:branches,id',
+            'color' => 'nullable|string|exists:product_details,color',
+            'quantity' => 'required|integer|min:1'
+        ];
+    }
+}
